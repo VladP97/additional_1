8986641e8043b066f93834e6322131b7c9820609
VladP97 <vlad.potapov1997@gmail.com>
2017-02-20 22:53:00 +0300

    additional_1/fix2

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 module.exports = function sum() {
-  var x = 0;
-  for (let i = 0; i < arguments.length; i++) {
-    x += +arguments[i];
-  }
-  return function(){
-    if (arguments.length!=0) {
-      var y = 0;
-      for (let i = 0; i < arguments.length; i++) {
-        y += +arguments[i];
-      }
-      x += y;
-      return arguments.callee;
+    var x = 0;
+    if (arguments.length != 0) {
+        for (let i = 0; i < arguments.length; i++) {
+            x += +arguments[i];
+        }
+        return function() {
+            if (arguments.length != 0) {
+                var y = 0;
+                for (let i = 0; i < arguments.length; i++) {
+                    y += +arguments[i];
+                }
+                x += y;
+                return arguments.callee;
+            } else return x;
+        }
     }
     else return x;
-  }
 }
